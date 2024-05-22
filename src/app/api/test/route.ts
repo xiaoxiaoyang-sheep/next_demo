0d4447b57a9d41c73268af8b7cc3e57b6279d7fd
赵晨炀 <904852749@qq.com>
2024-05-22 12:30:52 +0800

    feat: zod 做api校验

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,9 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
+import z from "zod";
 
-export const dynamic = "force-dynamic"; // 规避默认的Cache
+// export const dynamic = "force-dynamic"; // 规避默认的Cache
+
+const inputSchema = z.object({
+	name: z.string().min(3).max(6),
+	email: z.string().email(),
+});
 
 export function GET(request: NextRequest) {
-    return NextResponse.json({
-        a: "hello world"
+    const query = request.nextUrl.searchParams;
+
+    const name = query.get("name");
+    const email = query.get("email");
+
+    const result = inputSchema.safeParse({
+        name,
+        email
     })
+
+    if(result.success) {
+        return NextResponse.json(result.data);
+    } else {
+        return NextResponse.json({error: result.error});
+    }
+	
 }
