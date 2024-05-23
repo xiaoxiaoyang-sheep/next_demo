0b819097dc9b0f0bb25c00f1b1be84c82e411c02
赵晨炀 <904852749@qq.com>
2024-05-23 21:20:19 +0800

    feat: 美化上传按钮

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
 		const handler: UploadSuccessCallback<{}> = (file, resp) => {
 			if (file) {
 				console.log(file, resp);
-				
+
 				trpcPureClient.file.saveFile.mutate({
 					name: file.data instanceof File ? file.data.name : "test",
 					path: resp.uploadURL ?? "",
@@ -56,17 +56,19 @@ export default function Home() {
 	const { data: fileList, isPending } =
 		trpcClientReact.file.listFiles.useQuery();
 
-
-	usePasteFile({onFilePaste: (files) => {
-		uppy.addFiles(files.map((file) => ({
-			data: file
-		})))
-	}})
+	usePasteFile({
+		onFilePaste: (files) => {
+			uppy.addFiles(
+				files.map((file) => ({
+					data: file,
+				}))
+			);
+		},
+	});
 
 	return (
 		<div className="container mx-auto p-4">
-			<div>
-				<UploadButton uppy={uppy}></UploadButton>
+			<div className="flex justify-between items-center mb-4">
 				<Button
 					onClick={() => {
 						uppy.upload();
@@ -74,17 +76,22 @@ export default function Home() {
 				>
 					Upload
 				</Button>
+				<UploadButton uppy={uppy}></UploadButton>
 			</div>
 			{isPending && <div>Loading</div>}
 			<Dropzone uppy={uppy}>
 				{(draging) => (
-					<div className={cn(
-						"flex flex-wrap gap-4 relative",
-						draging && " border border-dashed"
-					)}>
-						{
-							draging && <div className=" absolute inset-0 bg-secondary/30 flex justify-center items-center text-3xl">Drop File Here to Upload</div>
-						}
+					<div
+						className={cn(
+							"flex flex-wrap gap-4 relative",
+							draging && " border border-dashed"
+						)}
+					>
+						{draging && (
+							<div className=" absolute inset-0 bg-secondary/30 flex justify-center items-center text-3xl">
+								Drop File Here to Upload
+							</div>
+						)}
 						{fileList?.map((file) => {
 							const isImage =
 								file.contentType.startsWith("image");
@@ -92,7 +99,7 @@ export default function Home() {
 								<div className=" w-56 h-56 flex justify-center items-center border">
 									{isImage ? (
 										<img
-											className=" max-h-56 max-w-56"
+											className=" max-h-[13.9rem] max-w-[13.9rem]"
 											src={file.url}
 											alt={file.name}
 										></img>
--- src/components/feature/UploadButton.tsx
+++ src/components/feature/UploadButton.tsx
@@ -1,19 +1,38 @@
 import Uppy from "@uppy/core";
+import { Plus } from "lucide-react";
+import { Button } from "../Button";
+import { useRef } from "react";
 
 export function UploadButton({ uppy }: { uppy: Uppy }) {
+	const inputRef = useRef<HTMLInputElement>(null);
+
 	return (
-		<input
-			type="file"
-			onChange={(e) => {
-				if (e.target.files) {
-					Array.from(e.target.files).forEach((file) => {
-						uppy.addFile({
-							data: file,
+		<>
+			<Button
+				variant="ghost"
+				onClick={() => {
+					if (inputRef.current) {
+						inputRef.current.click();
+					}
+				}}
+			>
+				<Plus />
+			</Button>
+			<input
+				className="fixed left-[-10000px]"
+				ref={inputRef}
+				type="file"
+				onChange={(e) => {
+					if (e.target.files) {
+						Array.from(e.target.files).forEach((file) => {
+							uppy.addFile({
+								data: file,
+							});
 						});
-					});
-				}
-			}}
-			multiple
-		></input>
+					}
+				}}
+				multiple
+			></input>
+		</>
 	);
 }
