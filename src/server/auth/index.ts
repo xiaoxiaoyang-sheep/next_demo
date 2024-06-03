95b8e598ebcd3d59236e44f08aee23ccdbf50e69
赵晨炀 <904852749@qq.com>
2024-06-03 14:04:25 +0800

    1

--- src/server/auth/index.ts
+++ src/server/auth/index.ts
@@ -31,7 +31,7 @@ export const authOptions: AuthOptions = {
 	providers: [
 		GithubProvider({
 			clientId: process.env.GITHUB_CLIENT_ID!,
-			clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+			clientSecret: process.env.GITHUB_CLIENT_SECR!,
 		}),
 		// CredentialsProvider({
 		// 	// The name to display on the sign in form (e.g. "Sign in with...")
